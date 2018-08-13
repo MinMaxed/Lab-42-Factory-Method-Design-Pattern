a7d4095b2ae4c7f571ddfc0e1b2b4d2975c990fc
Max <gusstorm132@gmail.com>
2018-08-12 18:42:31 -0700

    did the thing

---
+++ DesignPattern/DesignPattern/Classes/BookFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Classes
+{
+    public class BookFactory
+    {
+        /// <summary>
+        /// Contcrete Book maker based on type
+        /// </summary>
+        /// <param name="bookType">type of book to make</param>
+        /// <returns></returns>
+        public static Book CreateBook(string bookType)
+        {
+            Book book = null;
+            switch (bookType.ToLower())
+            {
+                case "scifi":
+                    book = new SciFi();
+                    break;
+                case "western":
+                    book = new Western();
+                    break;
+                case "mystery":
+                    book = new Mystery();
+                    break;
+                default:
+                    break;
+            }
+            return book;
+        }
+    }
+}
